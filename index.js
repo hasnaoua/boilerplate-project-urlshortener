dfe5dbe93d79132e7e5aaaa89ad98671f28d7776
hasnaoua <abdelhayhasnaoui@gmail.com>
2025-07-25 22:55:34 +0200

    add new endpoint

--- index.js
+++ index.js
@@ -8,17 +8,30 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+// Home route
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
+// First API endpoint
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  res.json({
+    'url': req.body.url,
+    'short_url': req.body.short_url
+  });
+})
+
+// Start server
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
